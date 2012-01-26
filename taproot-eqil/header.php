52cc24f0bea607480a45dc11fc0eadac5a80f8d2
Alex Chang <shortestpath@yahoo.com>
2012-01-25 19:17:40 -0600

    left-align and box level 1 menu items

--- taproot-eqil/header.php
+++ taproot-eqil/header.php
@@ -1,4 +1,3 @@
-
 <?php
 /**
  * The Header for our theme.
@@ -8,29 +7,26 @@
  * @package WordPress
  * @subpackage Taproot EQIL
  */
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
 <html <?php language_attributes(); ?>> 
-
-    <head> 
-        <title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
-
-        <meta charset="<?php bloginfo('charset'); ?>" />
-        <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-
-        <link rel="canonical" href="<?php bloginfo('url'); ?>"/> 
-        <link href="<?php bloginfo('stylesheet_url'); ?>" media="screen" rel="stylesheet" type="text/css" />
-        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/reset.css" />
-        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/text.css" />
-        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/960_24_col.css" />
-        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/sticky_footer.css" />
-        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/master.css" />
-        <?php wp_head(); ?>
-    </head> 
-
-
-    <body <?php body_class('container_24 wrapper'); ?>> 
-
-
+<head>
+    <title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
+    <meta charset="<?php bloginfo('charset'); ?>" />
+    <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+    <link rel="canonical" href="<?php bloginfo('url'); ?>"/> 
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/reset.css" />
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/text.css" />
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/960_24_col.css" />
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/sticky_footer.css" />
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/master_do_not_use.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/main.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/nav.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/modules.css" />
+    <?php wp_head(); ?>
+</head> 
+<body>
+    <div class="container_24 wrapper"> 
         <div id="header" class="grid_24 alpha omega">
             <form class="search grid_5 push_18 alpha" action=".">
                 <input class="text" type="text" name="search">
@@ -38,4 +34,3 @@
             </form>
         </div>
         <div class="clear"></div>
-
