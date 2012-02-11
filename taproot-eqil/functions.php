2d52ba156bf9dba38ccbaca5a9e31ed95f25d59b
Eddie Moya <eddie.moya@gmail.com>
2012-02-11 16:44:19 -0600

    added reset.css, did some minor styling, fixed the header to be full width, jquery animation for collapsing menu

--- taproot-eqil/functions.php
+++ taproot-eqil/functions.php
@@ -4,6 +4,10 @@
  * @package WordPress
  * @subpackage Birthday Club
  */
+
+// add_theme_support('post-thumbnails');
+
+
 if (function_exists('register_sidebar')) {
     
     register_sidebar(array(
@@ -17,10 +21,36 @@ if (function_exists('register_sidebar')) {
 }
 
 
-// 2012-01-22 AVC
-// Without the call to register_nav_menus, the wp-admin > Appearance > Menus page >
-// Theme Locations section will show that "The current theme does not natively support 
-// menus..."
+
+function enqueue_scripts() {
+    if (!is_admin()) { // do not enqueue on admin pages
+        
+        $template_directory =  get_template_directory_uri();
+        
+        wp_deregister_script('jquery');
+        wp_register_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js');
+        wp_enqueue_script('jquery');
+        
+        
+        wp_register_script('eqil_js', $template_directory . '/js/eqil.js');
+        wp_enqueue_script('eqil_js');
+    }
+}
+add_action('wp_enqueue_scripts', 'enqueue_scripts');
+
+
+
+function enqueue_styles() { 
+    wp_register_style('eqil-styles', get_stylesheet_uri());
+    //wp_enqueue_style( 'eqil-styles');
+}
+add_action('wp_enqueue_scripts', 'enqueue_styles');
+/**
+ *  2012-01-22 AVC
+ * Without the call to register_nav_menus, the wp-admin > Appearance > Menus page >
+ * Theme Locations section will show that "The current theme does not natively support 
+ * menus..."
+ */
 function register_my_menus() {
     register_nav_menus(
         array('main-navigation' => __( 'Main Navigation' ) )
---
+++ taproot-eqil/js/eqil.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function($){
+    $('#nav ul.menu > li.menu-item').on({
+        mouseenter: function(){
+            
+            if( $('.sub-menu', this).is(':hidden')) {
+                $('.sub-menu').slideUp();
+                $('.sub-menu',this).slideDown('fast');
+            }
+            
+        },
+        mouseleave: function(){
+            
+        }
+        
+    })
+});
+
