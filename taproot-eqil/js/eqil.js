993af4e5070e46acc6a16c6bfd3f2f8a077f63e8
Eddie Moya <eddie.moya@gmail.com>
2012-02-11 16:46:40 -0600

    deleted unnecessary bits of js

--- taproot-eqil/js/eqil.js
+++ taproot-eqil/js/eqil.js
@@ -2,16 +2,12 @@ jQuery(document).ready(function($){
     $('#nav ul.menu > li.menu-item').on({
         mouseenter: function(){
             
-            if( $('.sub-menu', this).is(':hidden')) {
+            if( $('.sub-menu', this).is(':hidden') ) {
+                
                 $('.sub-menu').slideUp();
                 $('.sub-menu',this).slideDown('fast');
             }
-            
-        },
-        mouseleave: function(){
-            
         }
-        
     })
 });
 
