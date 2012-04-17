24758b50b69f317a6cd4a8c639c60392dd9848ec
Eddie Moya <eddie.moya@gmail.com>
2012-04-16 21:48:06 -0500

    salsa labs email form

--- taproot-eqil/sidebar.php
+++ taproot-eqil/sidebar.php
@@ -6,29 +6,17 @@
 ?>
 
     <!-- start nav -->
-    <div id="nav" class="grid_3 alpha prefix_1">
+    <div id="nav" class="grid_2 alpha">
         <a class="logo" href="/"><img src="<?php echo get_template_directory_uri(); ?>/img/logo.eqil.gif"></a>
-
-        <div class="clear"></div>
-        <?php
-            wp_nav_menu(
-                array(
-                    'menu' => 'Main Navigation',
-                    'container_class' => 'rail_box outer nav',
-                    'menu_class' => 'rail_box menu level1' . (is_home() ? ' active gradient_offwhite' : '')
-                ) 
-            ); 
-        ?>
-
     </div>
     <!-- end nav -->
 
     <!-- start body -->
-    <div id="body" class="grid_14">
+    <div id="body" class="grid_17">
         <div id="carousel"><?php
-if( function_exists('FA_display_slider') ){
-    FA_display_slider(129);
-}
+        if( function_exists('FA_display_slider') ){
+            FA_display_slider(129);
+        }
 ?>
 </div>
 
