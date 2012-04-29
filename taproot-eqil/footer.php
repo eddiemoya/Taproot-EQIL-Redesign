16978b8d03acab79b2370f47ea690b2bfdf721fb
Eddie Moya <eddie.moya@gmail.com>
2012-04-29 16:23:53 -0500

    refactored part of the footer structure to allow for dynamic centering. related to issue #14

--- taproot-eqil/footer.php
+++ taproot-eqil/footer.php
@@ -6,69 +6,68 @@
 ?>
 
 
-        <!-- required for sticky footer -->
-        <div class="clear"></div>
-    </div>
-    <!-- end .container_24 wrapper -->
+            <!-- required for sticky footer -->
+            <div class="clear"></div>
+        </div>
+        <!-- end .container_24 wrapper -->
 
-    <div class="footer">
-        <?php wp_footer(); ?>
-        
+        <div class="footer">
+            <?php wp_footer(); ?>
 
-        <div class="grid_24 alpha omega container_24" id="footer_top">
-            <div id="footer_wrapper">
-            <div class="grid_4 alpha prefix_4">
-                <div class="header">About Us</div>
-                <div class="body">
-                    <a href="http://www.equalityillinois.us/about-us/accomplishments-mission/">Mission</a><br>
-                    <a href="http://www.equalityillinois.us/about-us/accomplishments-mission/">Accomplishments</a><br>
-                    <a href="http://www.equalityillinois.us/about-us/annual-reports/">Annual Reports</a><br>
-                    <a href="http://www.equalityillinois.us/about-us/team/">EQIL Team</a><br>
-                    <a href="http://www.equalityillinois.us/about-us/board-of-directors/">Board of Directors</a><br>
-                    <a href="http://www.equalityillinois.us/about-us/sponsors/">Sponsors</a><br>
+            <div class="grid_24 alpha omega container_24" id="footer_top">
+                <div id="footer_wrapper">
+                    <div class="grid_4 alpha prefix_4">
+                        <div class="header">About Us</div>
+                        <div class="body">
+                            <a href="http://www.equalityillinois.us/about-us/accomplishments-mission/">Mission</a><br>
+                            <a href="http://www.equalityillinois.us/about-us/accomplishments-mission/">Accomplishments</a><br>
+                            <a href="http://www.equalityillinois.us/about-us/annual-reports/">Annual Reports</a><br>
+                            <a href="http://www.equalityillinois.us/about-us/team/">EQIL Team</a><br>
+                            <a href="http://www.equalityillinois.us/about-us/board-of-directors/">Board of Directors</a><br>
+                            <a href="http://www.equalityillinois.us/about-us/sponsors/">Sponsors</a><br>
+                        </div>
+                    </div>
+                    <div class="grid_4">
+                        <div class="header">Our Work</div>
+                        <div class="body">
+                            <a href="http://www.equalityillinois.us/our-work/lobbying/">Lobbying</a>
+                            <a href="http://www.equalityillinois.us/our-work/education/">Education</a>
+                            <a href="http://www.equalityillinois.us/our-work/elections/">Elections</a>
+                            <a href="http://www.equalityillinois.us/our-work/ongoing-projects-initiatives/">Ongoing Projects & Initiatives</a>
+                        </div>
+                    </div>
+                    <div class="grid_4">
+                        <div class="header">Stay Informed</div>
+                        <div class="body">
+                            <a href=".">EQIL in the News</a><br>
+                            <a href=".">Recent Press Releases</a><br>
+                            <a href="http://equalityfederation.salsalabs.com/o/35010/p/salsa/web/common/public/signup?signup_page_KEY=21">Subscribe</a><br>
+                        </div>
+                    </div>
+                    <div class="grid_4 omega suffix_4">
+                        <div class="header">Get In Touch</div>
+                        <div class="body">
+                            <a href="mailto:info@eqil.org">info@eqil.org</a><br>
+                            <a href="http://facebook.com/equalityillinois">http://facebook.com/equalityillinois</a>
+                            <a href="http://twitter.com/EqualityILL">http://twitter.com/EqualityILL</a>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class="grid_4">
-                <div class="header">Our Work</div>
-                <div class="body">
-                    <a href="http://www.equalityillinois.us/our-work/lobbying/">Lobbying</a>
-                    <a href="http://www.equalityillinois.us/our-work/education/">Education</a>
-                    <a href="http://www.equalityillinois.us/our-work/elections/">Elections</a>
-                    <a href="http://www.equalityillinois.us/our-work/ongoing-projects-initiatives/">Ongoing Projects & Initiatives</a>
-                </div>
-            </div>
-            <div class="grid_4">
-                <div class="header">Stay Informed</div>
-                <div class="body">
-                    <a href=".">EQIL in the News</a><br>
-                    <a href=".">Recent Press Releases</a><br>
-                    <a href="http://equalityfederation.salsalabs.com/o/35010/p/salsa/web/common/public/signup?signup_page_KEY=21">Subscribe</a><br>
-                </div>
-            </div>
-            <div class="grid_4 omega suffix_4">
-                <div class="header">Get In Touch</div>
-                <div class="body">
-                    <a href="mailto:info@eqil.org">info@eqil.org</a><br>
-                    <a href="http://facebook.com/equalityillinois">http://facebook.com/equalityillinois</a>
-                    <a href="http://twitter.com/EqualityILL">http://twitter.com/EqualityILL</a>
-                </div>
-            </div>
+
+            <div class="clear"></div>
+
+            <div class="grid_24 alpha omega" id="footer_bottom">
+                <ul class="prefix_3">
+                    <li><a href="http://www.equalityillinois.us/about-us/privacy-policy/">Privacy Policy</a></li>
+                    <li><a href="http://www.equalityfederation.org/">Proud Member of the Equality Federation</a></li>
+                    <li><a href=".">Terms of Use</a></li>
+                    <li><a href="http://www.equalityillinois.us/about-us/eqil-political-action-committee/">Distinguish PAC vs. Org</a></li>
+                    <li><a href=".">Copyright 2011 Equality Illinois</a></li>
+                </ul>
             </div>
         </div>
+        <!-- end .container_24 footer -->
 
-        <div class="clear"></div>
-
-        <div class="grid_24 alpha omega" id="footer_bottom">
-            <ul class="prefix_3">
-                <li><a href="http://www.equalityillinois.us/about-us/privacy-policy/">Privacy Policy</a></li>
-                <li><a href="http://www.equalityfederation.org/">Proud Member of the Equality Federation</a></li>
-                <li><a href=".">Terms of Use</a></li>
-                <li><a href="http://www.equalityillinois.us/about-us/eqil-political-action-committee/">Distinguish PAC vs. Org</a></li>
-                <li><a href=".">Copyright 2011 Equality Illinois</a></li>
-            </ul>
-        </div>
-    </div>
-    <!-- end .container_24 footer -->
-
-</body>
+    </body>
 </html>
